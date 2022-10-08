834fdeb3a80d3c676e9db5355ecd666e33455546
qasem <taaheri@outlook.com>
2022-10-09 00:35:18 +0330

    fg

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime/debug"
+)
 
 func main() {
+
+	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Sum != "" {
+		fmt.Println(info)
+	}
+
 	fmt.Println("hello world")
 }
